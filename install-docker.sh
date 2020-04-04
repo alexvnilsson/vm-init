ba9d5a847a05819b97c3a01d2a5ddb112515cd22
Alexander Nilsson <alexvnilsson@outlook.com>
2020-04-04 11:35:10 +0200

    Update docker, node init scripts

--- install-docker.sh
+++ install-docker.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-sudo apt-get install \
+sudo apt-get install -y \
     apt-transport-https \
     ca-certificates \
     curl \
@@ -8,14 +8,10 @@ sudo apt-get install \
     software-properties-common
 
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-
 sudo apt-key fingerprint 0EBFCD88
-
 sudo add-apt-repository \
    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
    $(lsb_release -cs) \
    stable"
 
-sudo apt-get update
-
-sudo apt-get install docker-ce docker-ce-cli containerd.io
+sudo apt-get update && sudo apt-get install -y docker-ce docker-ce-cli containerd.io
